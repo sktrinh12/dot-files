9d0a46dc22f8b08f52bfde0d3c754cdd0c726241
sktrinh12 <sktrinh12@gmail.com>
2024-09-30 11:05:45 -0500

    add shotcut for load conf file and buffer list

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,7 @@ vim.opt.showcmd = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -26,3 +27,18 @@ vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', { noremap = true, silent = t
 -- Navigate to the previous buffer
 vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprevious<CR>', { noremap = true, silent = true })
 
+
+-- Use the 'fzf' command to show only filenames in the buffer list
+vim.api.nvim_set_keymap('n', '<leader>b', ":Buffers<CR>", { noremap = true, silent = true })
+
+-- Function to reload Neovim configuration
+function _G.reload_config()
+  vim.cmd('source ~/.config/nvim/init.lua')
+  print("Configuration reloaded!")
+end
+
+-- Key mapping to reload configuration
+vim.api.nvim_set_keymap('n', '<leader>l', [[:lua reload_config()<CR>]], { noremap = true, silent = true })
+
+-- Key mapping to change directory and open init.lua
+vim.api.nvim_set_keymap('n', '<leader>cd', ':cd ~/.config/nvim/lua/config<CR>:e options.lua<CR>', { noremap = true, silent = true })
