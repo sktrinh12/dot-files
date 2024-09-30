346f1f9a0fcbc0a60ce90c3a6eac029d9241a4e4
sktrinh12 <sktrinh12@gmail.com>
2024-09-30 13:34:04 -0500

    remove vim, lua, added python, bash, js, ts

--- lua/config/plugin_config/treesitter.lua
+++ lua/config/plugin_config/treesitter.lua
@@ -1,6 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "lua", "vim", "html" },
+  ensure_installed = { "bash", "javascript", "typescript", "python"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
