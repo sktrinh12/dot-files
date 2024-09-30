609387717bf660664e6a48bb69a4c680b23dbbea
sktrinh12 <sktrinh12@gmail.com>
2024-09-30 14:34:16 -0500

    bash script to crate symbolic links for fresh install

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s $HOME/Documents/scripts/dot-files/zshrc $HOME/.zshrc
+ln -s $HOME/Documents/scripts/dot-files/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/Documents/scripts/dot-files/nvim $HOME/.config/nvim
