3a8a2fdeee67b3ef948adc0ac583e6ff1a3130ba
sktrinh12 <sktrinh12@gmail.com>
2024-10-21 11:58:54 -0400

    call internal vim spell-check

--- nvim/lua/config/plugin_config/init.lua
+++ nvim/lua/config/plugin_config/init.lua
@@ -3,6 +3,7 @@ require("config.plugin_config.lualine")
 require("config.plugin_config.nvim-tree")
 require("config.plugin_config.treesitter")
 require("config.plugin_config.fzf")
+require("config.plugin_config.spelling")
 require("config.plugin_config.format")
 require('lspconfig').ts_ls.setup{}
 require('lspconfig').bashls.setup{}
