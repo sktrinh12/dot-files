fc13337a1000bd90e4183ac44424c305c02db341
sktrinh12 <sktrinh12@gmail.com>
2024-10-21 09:52:04 -0400

    use fzf-lua plugin instead of telescope

--- nvim/lua/config/plugin_config/init.lua
+++ nvim/lua/config/plugin_config/init.lua
@@ -2,7 +2,7 @@ require("config.plugin_config.gruvbox")
 require("config.plugin_config.lualine")
 require("config.plugin_config.nvim-tree")
 require("config.plugin_config.treesitter")
-require("config.plugin_config.telescope")
+require("config.plugin_config.fzf")
 require("config.plugin_config.format")
 require('lspconfig').ts_ls.setup{}
 require('lspconfig').bashls.setup{}
