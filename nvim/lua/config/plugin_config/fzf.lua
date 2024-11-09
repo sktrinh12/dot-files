1c2a4e32521f4a07a538f7a6dad4af0c61695e9a
sktrinh12 <sktrinh12@gmail.com>
2024-11-09 13:52:21 -0500

    move fzf keybindings to keymaps.lua

--- nvim/lua/config/plugin_config/fzf.lua
+++ nvim/lua/config/plugin_config/fzf.lua
@@ -18,8 +18,3 @@ require('fzf-lua').setup({
     },
   },
 })
-
-vim.keymap.set("n", "<leader>f", require('fzf-lua').files, { desc = "Fzf Files" })
-vim.keymap.set("n", "<leader>rg", require('fzf-lua').live_grep, { desc = "Fzf Grep" })
-vim.keymap.set("n", "<leader>b", require('fzf-lua').buffers, { desc = "Fzf Buffers" })
-vim.keymap.set("n", "<leader>r", require('fzf-lua').registers, { desc = "Fzf Registers" })
