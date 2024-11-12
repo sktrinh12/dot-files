8922e34faf59aa9cf89f64733e0a355604044829
sktrinh12 <sktrinh12@gmail.com>
2024-11-12 13:26:59 -0500

    change to allow passing of text

--- zsh_fx/cowsay-prompt.sh
+++ zsh_fx/cowsay-prompt.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ "$#" -gt 0 ]; then
+  # If an argument is passed, use it as the body of text
+  text="$*"
+else
+  # Otherwise, use the output of the `fortune` command
+  text=$(/usr/games/fortune)
+fi
+
 # make into list and randomly pick one
-cowselect="$(cowsay -l | sed "1 d" | tr ' ' '\n' | shuf -n 1)"
-eng_text=$(fortune)
-echo ${eng_text} | cowsay -f "$cowselect" -W50 | lolcat
+cowselect="$(/usr/games/cowsay -l | sed "1 d" | tr ' ' '\n' | /usr/bin/shuf -n 1)"
+echo ${text} | /usr/games/cowsay -f "$cowselect" -W 10 | /usr/games/lolcat
