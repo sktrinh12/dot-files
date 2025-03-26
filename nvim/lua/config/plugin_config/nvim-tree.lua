169557fac612ab5800d4f3c09a9e40da720c5eb1
sktrinh12 <strkinh12@gmail.com>
2025-03-26 16:01:10 -0400

    allow to view ignored files in tree

--- nvim/lua/config/plugin_config/nvim-tree.lua
+++ nvim/lua/config/plugin_config/nvim-tree.lua
@@ -1,4 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+ filters = {
+    dotfiles = false,
+    git_ignored = false,
+  }
+})
