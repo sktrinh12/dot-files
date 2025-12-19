859bf59b4d91bd14b386a754832bb934af6c8f6f
sktrinh12 <strkinh12@gmail.com>
2025-12-19 15:05:41 -0500

    add syntax highlighting for nextflow

--- nvim/lua/config/plugin_config/treesitter.lua
+++ nvim/lua/config/plugin_config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "bash", "javascript", "typescript", "python", "markdown", "sql"},
+  ensure_installed = { "bash", "javascript", "typescript", "python", "markdown", "sql", "groovy",},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
