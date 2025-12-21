10576366738d5209feb0003aa75524feae3b5eba
sktrinh12 <strkinh12@gmail.com>
2025-12-21 15:08:03 -0500

    add nextflow icon glyph from nerdfont

--- nvim/lua/config/plugin_config/nvim-tree.lua
+++ nvim/lua/config/plugin_config/nvim-tree.lua
@@ -1,6 +1,17 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local devicons = require('nvim-web-devicons')
+devicons.setup({
+  override = {
+    nf = {
+      icon = "󰚄",
+      color = "#04c19e",
+      name = "Nextflow"
+    }
+  }
+})
+
 require("nvim-tree").setup({
  filters = {
     dotfiles = false,
